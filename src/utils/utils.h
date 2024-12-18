a019c8458eed19034f3e1a98e8a1c14147834c2b
FusionSid <siddheshadsv@icloud.com>
2024-12-19 00:43:34 +1300

    feat: add something to print array with seperator

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,9 +1,24 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <iterator>
 #include <string>
 #include <vector>
+#include <iostream>
 
 std::vector<std::string> readInputFile(const std::string& filename);
+
+template <typename Container>
+void print_arr(const Container& array, std::string sep = " ") {
+    auto it = array.begin();
+    while (it != array.end()) {
+        std::cout << *it;
+        ++it;
+        if (it != array.end()) {
+            std::cout << sep;
+        }
+    }
+    std::cout << std::endl;
+}
 
 #endif
